51f7d37291dfaa79f6c828840982efccfefa7165
Kyle <105106718+KyleWarren08@users.noreply.github.com>
2022-08-09 11:57:31 +0200

    Add files via upload

---
+++ Stock_Taking_Web_API/Stock_Taking_Web_API/Migrations/20220804090627_Intial Migration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stock_Taking_Web_API.Migrations
+{
+    public partial class IntialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stock",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CarMakeModel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CarAmount = table.Column<int>(type: "int", nullable: false),
+                    CarFeatures = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsVisable = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stock", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stock");
+        }
+    }
+}
---
+++ Stock_Taking_Web_API/Stock_Taking_Web_API/Models/Entities/Stock.cs
@@ -0,0 +1,12 @@
+﻿namespace Stock_Taking_Web_API.Models.Entities
+{
+    public class Stock
+    {
+
+        public Guid Id { get; set; }
+        public string CarMakeModel { get; set; }    
+        public int CarAmount { get; set; } 
+        public string CarFeatures{ get; set; }  
+        public bool IsVisable{ get; set; } 
+    }
+}
